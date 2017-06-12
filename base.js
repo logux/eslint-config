ab675094cf8b26066c5805fa1f62a335ca01e2b4
Andrey Sitnik <andrey@sitnik.ru>
2017-06-12 20:16:27 -0300

    Add for-direction rule

--- base.js
+++ base.js
@@ -37,6 +37,7 @@ module.exports = {
     'no-invalid-this': 'error',
     'global-require': 'error',
     'no-unused-vars': [2, { vars: 'all', args: 'after-used' }],
+    'for-direction': 'error',
     'no-lonely-if': 'error',
     'valid-jsdoc': 'error',
     'func-style': ['error', 'declaration'],
