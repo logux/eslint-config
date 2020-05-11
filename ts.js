dbc97855b412bcc049f7ad051c1570c1749d77ee
Andrey Sitnik <andrey@sitnik.ru>
2020-05-10 22:03:08 -0400

    Remove ESLint recommended rules from TS

--- ts.js
+++ ts.js
@@ -16,7 +16,6 @@ module.exports = {
     {
       files: ['*.ts'],
       extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking'
       ],
