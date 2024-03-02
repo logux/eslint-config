1259ad49e806aa35fc1492d39d5c1cfe5bb171b2
Andrey Sitnik <andrey@sitnik.ru>
2024-03-02 13:58:08 +0100

    Fix tests

--- demo/test.js
+++ demo/test.js
@@ -4,7 +4,9 @@ import { join } from 'node:path'
 
 let expected = readFileSync(join(import.meta.dirname, 'expected.out'))
 
-exec(`pnpm eslint "${join(import.meta.dirname, 'index.*')}"`, (_, stdout) => {
+const FILES = join(import.meta.dirname, 'index.*')
+
+exec(`pnpm eslint --no-color ${FILES}`, (_, stdout) => {
   if (stdout !== expected.toString()) {
     process.stderr.write(`Expected: ${expected}`)
     process.stderr.write(`Actual: ${stdout}`)
