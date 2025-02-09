2b97e0310bf68ef32038ccfad46c413a6739db79
Andrey Sitnik <andrey@sitnik.ru>
2025-02-09 22:13:38 +0000

    Add no-misused-spread and no-unsafe-type-assertion TS rules

--- ts.js
+++ ts.js
@@ -41,6 +41,7 @@ export default [
         'error',
         { capIsConstructor: true }
       ],
+      '@typescript-eslint/no-misused-spread': 'error',
       '@typescript-eslint/no-mixed-enums': 'error',
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
       '@typescript-eslint/no-redeclare': 'error',
@@ -48,6 +49,7 @@ export default [
       '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
       '@typescript-eslint/no-unnecessary-condition': 'error',
       '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unsafe-type-assertion': 'error',
       '@typescript-eslint/no-unused-expressions': [
         'error',
         {
