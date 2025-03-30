d25529d6bbc4ee23884b5945fbb4d76dbd7fe80f
Andrey Sitnik <andrey@sitnik.ru>
2025-03-30 14:23:31 +0000

    Make it easier to write tests

--- ts.js
+++ ts.js
@@ -90,15 +90,17 @@ export default [
     }
   },
   {
-    files: ['*.{test.ts,test.tsx,stories.tsx}', 'types.ts', '**/test/*'],
+    files: ['**/*.{test.ts,test.tsx,stories.tsx}', 'types.ts', '**/test/*'],
     name: 'logux/ts-tests',
     rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-type-assertion': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/restrict-plus-operands': 'off',
@@ -109,6 +111,7 @@ export default [
     files: ['**/types.ts'],
     name: 'logux/type-tests',
     rules: {
+      '@typescript-eslint/no-floating-promises': 'off',
       'no-console': 'off'
     }
   },
